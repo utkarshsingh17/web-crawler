36e64779feaf15f1aecb26918edc30f3ce78b014
utkarsh-dev <utkarsh@zooper.ai>
2026-05-07 15:54:12 +0530

    initial commit

---
+++ src/main/java/com/utkarsh/web_crawler/WebCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsh.web_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCrawlerApplication.class, args);
+	}
+
+}
